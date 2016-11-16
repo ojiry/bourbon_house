957caaba704c4bb9c23b88e0d4791c132495ae68
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2016-11-16 20:44:54 +0900

    Add spec

---
+++ spec/bourbon_house/bartender_spec.rb
@@ -0,0 +1,23 @@
+describe BourbonHouse::Bartender do
+  describe ".message" do
+    subject { BourbonHouse::Bartender.message }
+
+    specify do
+      is_expected.to eq <<~HTML
+        やあ （´・ω・｀)
+        ようこそ、バーボンハウスへ。
+        このテキーラはサービスだから、まず飲んで落ち着いて欲しい。
+        
+        うん、「また」なんだ。済まない。
+        仏の顔もって言うしね、謝って許してもらおうとも思っていない。
+        
+        でも、このレスポンスを見たとき、君は、きっと言葉では言い表せない
+        「ときめき」みたいなものを感じてくれたと思う。
+        殺伐とした世の中で、そういう気持ちを忘れないで欲しい
+        そう思って、このライブラリを作ったんだ。
+        
+        じゃあ、注文を聞こうか。
+      HTML
+    end
+  end
+end
--- spec/bourbon_house_spec.rb
+++ spec/bourbon_house_spec.rb
@@ -1,11 +1,5 @@
-require "spec_helper"
-
 describe BourbonHouse do
   it "has a version number" do
     expect(BourbonHouse::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
