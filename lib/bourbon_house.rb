728c29e12cca61ad38388300162bef4227b64ba7
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2016-12-21 20:42:56 +0900

    Modify require order and check

--- lib/bourbon_house.rb
+++ lib/bourbon_house.rb
@@ -1,14 +1,20 @@
 # frozen_string_literal: true
+module BourbonHouse
+end
+
+begin
+  require "rails"
+rescue LoadError
+  # noop
+end
+
 require "bourbon_house/config"
 require "bourbon_house/application"
 require "bourbon_house/bartender"
 require "bourbon_house/server"
+require "bourbon_house/middleware"
 
-if defined?(Rails)
-  require 'bourbon_house/middleware'
-  require 'bourbon_house/railtie'
+if defined? ::Rails::Railtie
+  require "bourbon_house/railtie"
 end
 
-module BourbonHouse
-  # Your code goes here...
-end
